fcd895f0297437ab9009d49edfa5e30c7dd861f0
Kevin <knphillips01@gmail.com>
2020-01-17 11:19:23 -0600

    got response back from axios request for Cards

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -21,7 +21,7 @@ const topics = document.querySelector('.topics');
 axios
   .get('https://lambda-times-backend.herokuapp.com/topics')
   .then(response => {
-    console.log(response.data.topics); 
+    //console.log(response.data.topics); 
       response.data.topics.forEach(item => {
         const newTab = document.createElement('div');
         newTab.classList.add('tab');
