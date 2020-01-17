3f4398e702daa5ddd4287f7d0e027fd677be42c1
Kevin <knphillips01@gmail.com>
2020-01-17 10:49:11 -0600

    set up tabs function

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,11 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+function Tabs(obj) {
+  const tab = document.createElement('div');
+  
+  tab.classList.add('tab');
+
+  return tab; 
+}
